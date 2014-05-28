95a447368d79ae5571d3b5e07b9db6c70abdcd78
Alexander Novikov <anovikov@granpm.com>
2014-05-28 04:00:18 -0700

    add history tests

--- tests/unit/HistoryTest.php
+++ tests/unit/HistoryTest.php
@@ -5,8 +5,25 @@ use Pubnub\Pubnub;
 class HistoryTest extends TestCase
 {
     protected $channel = 'pubnub_php_test_history';
+    protected $bootstrap;
+    protected $start;
     protected static $message = 'Hello from history() test!';
     protected static $message_2 = 'Hello from history() test! 2';
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->start = $this->pubnub->time();
+        $bootstrap = time();
+
+        for ($i = 0; $i < 10; $i++) {
+            $this->pubnub->publish(array(
+                'channel' => $this->channel,
+                'message' => "${bootstrap}#${i}"
+            ));
+        }
+    }
 
     public function testHistoryMessages()
     {
@@ -20,14 +37,40 @@ class HistoryTest extends TestCase
             'message' => static::$message_2
         ));
 
+        sleep(3);
+
         $response = $this->pubnub->history(array(
             'channel' => $this->channel,
             'count' => 2
         ));
 
         $this->assertCount(2, $response['messages']);
-        $this->assertEquals($response['messages'][0], static::$message);
-        $this->assertEquals($response['messages'][1], static::$message_2);
+        $this->assertEquals(static::$message, $response['messages'][0]);
+        $this->assertEquals(static::$message_2, $response['messages'][1]);
+    }
+
+    public function testHistoryIncludeToken()
+    {
+        $response = $this->pubnub->history(array(
+            'channel' => $this->channel,
+            'count' => 2,
+            'include_token' => true
+        ));
+
+        $this->assertArrayHasKey('message', $response['messages'][0]);
+        $this->assertArrayHasKey('timetoken', $response['messages'][0]);
+    }
+
+    public function testHistoryReverse()
+    {
+        $response = $this->pubnub->history(array(
+            'channel' => $this->channel,
+            'count' => 12,
+            'start' => $this->start,
+            'reverse' => true
+        ));
+
+        $this->assertRegExp('/#0$/', $response['messages'][0]);
     }
 
     public function testHistoryInvalidChannel()
