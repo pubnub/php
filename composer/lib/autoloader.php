ef981549bf7e8c787d6baa7f423feeee9cebe3fe
Alexander Novikov <anovikov@granpm.com>
2014-06-18 07:45:14 -0700

    update autoloader

--- composer/lib/autoloader.php
+++ composer/lib/autoloader.php
@@ -1,18 +1,22 @@
 <?php
-/*
- * Simple autoload for stanalone lib
- */
 
 define('PUBNUB_LIB_BASE_DIR', __DIR__);
 
-//autoloader
 function pubnubAutoloader($className)
 {
-    $classPath = str_replace('\\', '/', $className);
-    $filePath = sprintf('%s/%s.php', PUBNUB_LIB_BASE_DIR, $classPath);
-    if (file_exists($filePath)) {
-        require_once $filePath;
+    $className = ltrim($className, '\\');
+    $fileName  = '';
+    $namespace = '';
+
+    if ($lastNsPos = strrpos($className, '\\')) {
+        $namespace = strtolower(substr($className, 0, $lastNsPos));
+        $className = substr($className, $lastNsPos + 1);
+        $fileName .= str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
     }
+
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+
+    require_once $fileName;
 }
 
 spl_autoload_register('pubnubAutoloader', true, true);
