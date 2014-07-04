e4c11bc0ce484a0d140384b2bc8ca2506214ff4f
Alexander Novikov <anovikov@granpm.com>
2014-07-04 09:11:55 -0700

    fix json_decode in PHP v5.3

--- composer/lib/Pubnub/Pubnub.php
+++ composer/lib/Pubnub/Pubnub.php
@@ -672,10 +672,10 @@ class Pubnub
      * @param int $options
      * @return mixed
      */
-    private static function decode($val, $assoc = true, $depth = 512, $options = 0)
+    private static function decode($val, $assoc = true, $depth = 512)
     {
 
-        return json_decode($val, $assoc, $depth, $options);
+        return json_decode($val, $assoc, $depth);
 
     }
 }
