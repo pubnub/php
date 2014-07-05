85901d410be96e029f9763d9e11691eb6815276f
Alexander Novikov <anovikov@granpm.com>
2014-07-04 22:06:17 -0700

    fix composer version autoloader without explicit composer usage

--- composer/lib/autoloader.php
+++ composer/lib/autoloader.php
@@ -1,16 +1,20 @@
 <?php
+
+define('PUBNUB_LIB_BASE_DIR', __DIR__);
+
 function pubnubAutoloader($className)
 {
     $className = ltrim($className, '\\');
     $fileName  = '';
 
     if ($lastNsPos = strrpos($className, '\\')) {
-        $namespace = strtolower(substr($className, 0, $lastNsPos));
+        $namespace = substr($className, 0, $lastNsPos);
         $className = substr($className, $lastNsPos + 1);
         $fileName .= str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
     }
 
     $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+    $fileName = PUBNUB_LIB_BASE_DIR.DIRECTORY_SEPARATOR.$fileName;
 
     require_once $fileName;
 }
