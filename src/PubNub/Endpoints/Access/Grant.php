36998105c180a9714d78ab836f085bdc2f005ab7
f3cp <jozz@f3cp.com.au>
2020-08-31 13:40:41 +1000

    Update Grant.php
    Update grant to allow setting delete permission

--- src/PubNub/Endpoints/Access/Grant.php
+++ src/PubNub/Endpoints/Access/Grant.php
@@ -32,6 +32,9 @@ class Grant extends Endpoint
     /** @var  bool */
     protected $manage;
 
+    /** @var  bool */
+    protected $delete;
+
     /** @var  int */
     protected $ttl;
 
@@ -98,6 +101,17 @@ class Grant extends Endpoint
 
         return $this;
     }
+    
+    /**
+     * @param bool $flag
+     * @return $this
+     */
+    public function delete($flag)
+    {
+        $this->delete = $flag;
+
+        return $this;
+    }
 
     /**
      * Set time in minutes for which granted permissions are valid
@@ -148,6 +162,10 @@ class Grant extends Endpoint
 
         if ($this->manage !== null) {
             $params["m"] = ($this->manage) ? "1" : "0";
+        }
+
+        if ($this->delete !== null) {
+            $params["d"] = ($this->delete) ? "1" : "0";
         }
 
         if (count($this->authKeys) > 0) {
