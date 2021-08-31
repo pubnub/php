e3e4d98e6b21303988e051da93d503c59d58f386
Tim <treehousetim@gmail.com>
2021-08-31 13:04:37 -0500

    adding FCM to enums

--- src/PubNub/Enums/PNPushType.php
+++ src/PubNub/Enums/PNPushType.php
@@ -9,4 +9,6 @@ class PNPushType
     const APNS2 = "apns2";
     const MPNS = "mpns";
     const GCM = "gcm";
+    // FCM is the new name of GCM. Pubnub still requires 'gcm'
+    const FCM = "gcm";
 }
