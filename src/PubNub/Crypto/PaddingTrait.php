fa5dc2607724033618761a7def84aa6bc88bb9a7
Sebastian Molenda <sebastian.molenda@pubnub.com>
2023-10-02 15:37:38 +0200

    Crypto Module

---
+++ src/PubNub/Crypto/PaddingTrait.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace PubNub\Crypto;
+
+trait PaddingTrait
+{
+    public const BLOCK_SIZE = 16;
+
+    /**
+     * Pad $text to multiple of $blockSize lenght using PKCS5Padding schema
+     *
+     * @param string $text
+     * @param int $blockSize
+     * @return string
+     */
+    public function pad(string $text, int $blockSize = self::BLOCK_SIZE)
+    {
+        $pad = $blockSize - (strlen($text) % $blockSize);
+        return $text . str_repeat(chr($pad), $pad);
+    }
+
+    /**
+     * Remove padding from $text using PKCS5Padding schema
+     *
+     * @param string $text
+     * @param int $blockSize
+     * @return string
+     */
+    public function depad($data, $blockSize = self::BLOCK_SIZE)
+    {
+        $length = strlen($data);
+        if ($length == 0) {
+            return $data;
+        }
+
+        $padLength = substr($data, -1);
+
+        if (ord($padLength) <= $blockSize) {
+            for ($i = $length - 2; $i > 0; $i--) {
+                if (ord($data [$i] != $padLength)) {
+                    break;
+                }
+            }
+            return substr($data, 0, $i + 1);
+        }
+        return $data;
+    }
+}
---
+++ tests/unit/CryptoModule/PaddingTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace PubNubTests\unit\CryptoModule;
+
+use Generator;
+use PHPUnit\Framework\ExpectationFailedException;
+use PHPUnit\Framework\TestCase;
+use PubNub\Crypto\LegacyCryptor;
+use SebastianBergmann\RecursionContext\InvalidArgumentException;
+
+class PaddingTest extends TestCase
+{
+    protected LegacyCryptor $cryptor;
+    protected function setUp(): void
+    {
+        $this->cryptor = new LegacyCryptor("myCipherKey", false);
+    }
+
+    /**
+     * @dataProvider padProvider
+     * @param string $plain
+     * @param string $padded
+     * @return void
+     * @throws InvalidArgumentException
+     * @throws ExpectationFailedException
+     */
+    public function testPad(string $plain, string $padded): void
+    {
+        $this->assertEquals($this->cryptor->pad($plain), $padded);
+    }
+
+    /**
+     * @dataProvider depadProvider
+     * @param string $padded
+     * @param string $expected
+     * @return void
+     * @throws InvalidArgumentException
+     * @throws ExpectationFailedException
+     */
+    public function testDepad(string $padded, string $expected): void
+    {
+        $this->assertEquals($this->cryptor->depad($padded), $expected);
+    }
+
+    public function padProvider(): Generator
+    {
+        yield ["123456789012345", "123456789012345\x01"];
+        yield ["12345678901234", "12345678901234\x02\x02"];
+        yield ["1234567890123456", "1234567890123456" . str_repeat("\x10", 16)];
+    }
+
+    public function depadProvider(): Generator
+    {
+        yield ["123456789012345\x01", "123456789012345"];
+        yield ["12345678901234\x02\x02", "12345678901234"];
+        yield ["1234567890123456" . str_repeat("\x10", 16), "1234567890123456"];
+        yield ["1234567890123456", "1234567890123456"];
+    }
+}
