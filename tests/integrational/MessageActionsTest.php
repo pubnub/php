1168be233883541d46240058d5d14b49d98bc18f
Sebastian Molenda <sebastian.molenda@pubnub.com>
2025-01-02 13:30:24 +0100

    Add setup and teardown for tests

--- tests/integrational/MessageActionsTest.php
+++ tests/integrational/MessageActionsTest.php
@@ -2,79 +2,96 @@
 
 namespace Tests\Integrational;
 
+use PubNub\Models\Consumer\MessageActions\PNAddMessageActionResult;
 use PubNub\Models\Consumer\MessageActions\PNGetMessageActionResult;
 use PubNub\Models\Consumer\MessageActions\PNMessageAction;
 use PubNub\Models\Consumer\MessageActions\PNRemoveMessageActionResult;
 
 class MessageActionsTest extends \PubNubTestCase
 {
-    public function testAddMessageAction(): void
+    protected string $channelName = "pizza_talks";
+    protected int $messageTimetoken;
+
+    public function setUp(): void
     {
+        parent::setUp();
         $publishResult = $this->pubnub->publish()
-        ->channel("pizza_talks")
+        ->channel($this->channelName)
         ->message("Pineapple DOES belong on pizza!")
         ->sync();
 
-        $messageTimetoken = (int)$publishResult->getTimetoken();
+        $this->messageTimetoken = (int)$publishResult->getTimetoken();
+    }
 
+    public function tearDown(): void
+    {
+        parent::tearDown();
+        $this->pubnub->deleteMessages()
+            ->channel($this->channelName)
+            ->sync();
+    }
+
+    private function addTestMessageAction(): PNAddMessageActionResult
+    {
         $messageAction = new PNMessageAction([
             "type" => "reaction",
             "value" => "angry_face",
-            "messageTimetoken" => $messageTimetoken
+            "messageTimetoken" => $this->messageTimetoken
         ]);
 
         $addMessageActionResult = $this->pubnub->addMessageAction()
-            ->channel("pizza_talks")
+            ->channel($this->channelName)
             ->messageAction($messageAction)
             ->sync();
+        return $addMessageActionResult;
+    }
 
+    public function testAddMessageAction(): void
+    {
+        $addMessageActionResult = $this->addTestMessageAction();
         $this->assertNotNull($addMessageActionResult);
         $this->assertInstanceOf(PNMessageAction::class, $addMessageActionResult);
         $this->assertEquals("reaction", $addMessageActionResult->type);
         $this->assertEquals("angry_face", $addMessageActionResult->value);
-        $this->assertEquals($messageTimetoken, $addMessageActionResult->messageTimetoken);
+        $this->assertEquals($this->messageTimetoken, $addMessageActionResult->messageTimetoken);
     }
 
     public function testGetMessageAction(): void
     {
-        $publishResult = $this->pubnub->publish()
-        ->channel("pizza_talks")
-        ->message("Pineapple DOES belong on pizza!")
-        ->sync();
-
-        $messageTimetoken = (int)$publishResult->getTimetoken();
-
-        $messageAction = new PNMessageAction([
-            "type" => "reaction",
-            "value" => "angry_face",
-            "messageTimetoken" => $messageTimetoken
-        ]);
-
+        $addMessageActionResult = $this->addTestMessageAction();
         $getMessageActionResult = $this->pubnub->getMessageAction()
-            ->channel("pizza_talks")
+            ->channel($this->channelName)
             ->sync();
 
         $this->assertNotNull($getMessageActionResult);
         $this->assertInstanceOf(PNGetMessageActionResult::class, $getMessageActionResult);
+        $this->assertNotEmpty($getMessageActionResult->actions);
+        $this->assertCount(1, $getMessageActionResult->actions);
 
         $messageAction = $getMessageActionResult->actions[0];
         $this->assertInstanceOf(PNMessageAction::class, $messageAction);
         $this->assertEquals("reaction", $messageAction->type);
         $this->assertEquals("angry_face", $messageAction->value);
+        $this->assertEquals($this->messageTimetoken, $messageAction->messageTimetoken);
+        $this->assertEquals($addMessageActionResult->actionTimetoken, $messageAction->actionTimetoken);
     }
 
     public function testDeleteMessageAction(): void
     {
+        $addMessageActionResult = $this->addTestMessageAction();
+
         $getMessageActionResult = $this->pubnub->getMessageAction()
-            ->channel("pizza_talks")
+            ->channel($this->channelName)
             ->sync();
 
         $this->assertNotNull($getMessageActionResult);
         $this->assertInstanceOf(PNGetMessageActionResult::class, $getMessageActionResult);
+        $this->assertNotEmpty($getMessageActionResult->actions);
+        $this->assertCount(1, $getMessageActionResult->actions);
 
         foreach ($getMessageActionResult->actions as $action) {
             $removeMessageActionResult = $this->pubnub->removeMessageAction()
-                ->channel("pizza_talks")
+                ->channel($this->channelName)
                 ->messageTimetoken($action->messageTimetoken)
                 ->actionTimetoken($action->actionTimetoken)
                 ->sync();
