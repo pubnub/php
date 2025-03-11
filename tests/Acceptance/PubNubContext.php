36258c8cefcf0824d17aefed74d53c3a59d59499
Sebastian Molenda <sebastian.molenda@pubnub.com>
2025-03-11 10:54:26 +0100

    add wait for mock server to acceptance tests

--- tests/Acceptance/PubNubContext.php
+++ tests/Acceptance/PubNubContext.php
@@ -7,9 +7,12 @@ use Behat\Behat\Hook\Scope\BeforeScenarioScope;
 
 class PubNubContext
 {
+    protected int $retryLimit = 12;
+
     /** @BeforeScenario */
     public function before(BeforeScenarioScope $scope): void
     {
+        $this->waitForServer();
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
@@ -27,6 +30,7 @@ class PubNubContext
     /** @AfterScenario */
     public function after(AfterScenarioScope $scope): void
     {
+        $this->waitForServer();
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
@@ -38,5 +42,31 @@ class PubNubContext
             }
         }
         curl_close($ch);
+    }
+
+    protected function waitForServer()
+    {
+
+        for ($i = 1; $i <= $this->retryLimit; $i++) {
+            print("Trying to connect ($i/$this->retryLimit)...\n");
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+            curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+            curl_setopt($ch, CURLOPT_URL, 'http://localhost:8090');
+            curl_exec($ch);
+            $err = curl_error($ch);
+            if ($err === "") {
+                print("Server started\n");
+                break;
+            }
+            print($err . "\n");
+
+            if ($i === $this->retryLimit) {
+                print("Server not started\n");
+                exit(1);
+            }
+            sleep($i);
+        }
     }
 }
